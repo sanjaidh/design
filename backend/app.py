f61183ffb67be7c3fcb9b77fa33a1f366665ecdf
sanjaidh <sanjaidh2006@gmail.com>
2026-05-07 13:23:27 +0530

    fix flask static folder

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,7 @@ try:
 except ImportError:
     http_requests = None
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend', static_url_path='')
 # Enable CORS for all routes
 CORS(app)
 
